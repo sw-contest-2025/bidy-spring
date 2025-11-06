9c23d271b5668cc4594a0c205f830f8b25a86140
kayubin <kayubin@gmail.com>
2025-11-07 00:41:16 +0900

    feat: 채팅 통신 로직 수정

--- src/main/java/com/bidy/chat/entity/ChatRoomEntity.java
+++ src/main/java/com/bidy/chat/entity/ChatRoomEntity.java
@@ -1,10 +1,8 @@
 package com.bidy.chat.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import com.bidy.member.domain.Member;
 
 import java.time.LocalDateTime;
 
@@ -17,13 +15,26 @@ import java.time.LocalDateTime;
 public class ChatRoomEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 채팅방 고유 id
     private Long id;
 
+    // 경매 게시글 id -> Product 참조하면 될듯
     private Long auctionId;
-    private Long sellerId;
-    private Long buyerId;
-    private LocalDateTime createdAt;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    // 판매자 회원 참조
+    private Member seller;
+
+    // 구매자 회원 참조
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id")
+    private Member buyer;
+
+    // 채팅방 생성 시간
+    private LocalDateTime createdAt;
+    // 마지막 메시지
     private String lastMessage;
+    // 마지막 메시지 시간
     private LocalDateTime lastMessageTime;
 }
--- src/main/java/com/bidy/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/bidy/chat/repository/ChatMessageRepository.java
@@ -1,6 +1,7 @@
 package com.bidy.chat.repository;
 
 import com.bidy.chat.entity.ChatMessageEntity;
+import com.bidy.chat.entity.ChatRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity,Long> {
-    List<ChatMessageEntity> findByChatRoomIdOrderByCreatedAtAsc(Long roomId);
+    // 한 채팅방에 대한 메시지를 올림차순(오래된 순서)으로 가져오기
+    List<ChatMessageEntity> findByChatRoomOrderByCreatedAtAsc(ChatRoomEntity chatRoom);
 }
