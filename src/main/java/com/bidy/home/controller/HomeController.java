976f5e8d18098ae74b4139886c488aae1459ed14
chaerin <s.chaerlyn@gmail.com>
2025-11-09 20:30:40 +0900

    fix:마이페이지에서 계정설정으로 연결 에러 수정

--- src/main/java/com/bidy/home/controller/HomeController.java
+++ src/main/java/com/bidy/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.bidy.home.HomeController;
+package com.bidy.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "home"; // templates/home.html
     }
